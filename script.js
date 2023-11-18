af0281dec8ad93b443f6045906c264a305b70e27
kev-69 <bismarkobuobi19@gmail.com>
2023-11-18 16:04:08 +0000

    Made some change to the indentation

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 <script>
-                    var selectedItems = [];
+    var selectedItems = [];
                   
-                    function updateSelectedItems() {
-                      selectedItems = [];
-                      var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-                      for (var i = 0; i < checkboxes.length; i++) {
-                        if (checkboxes[i].checked) {
-                          selectedItems.push(checkboxes[i].value);
-                        }
-                      }
-                    }
+        function updateSelectedItems() {
+        selectedItems = [];
+        var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+        for (var i = 0; i < checkboxes.length; i++) {
+            if (checkboxes[i].checked) {
+        selectedItems.push(checkboxes[i].value);
+        }
+        }
+                    
+        }
                   
-                    function preventDeselection(checkbox) {
-                      if (selectedItems.length >= 3 && !checkbox.checked) {
-                        checkbox.checked = true;
-                        alert('You can only select a maximum of three items.');
-                      }
-                    }
+        function preventDeselection(checkbox) {
+            if (selectedItems.length >= 3 && !checkbox.checked) {
+        checkbox.checked = true;
+            alert('You can only select a maximum of three items.');
+        }
+                    
+        }
                   
-                    document.querySelector('#coursesList').addEventListener('change', function(event) {
-                      var checkbox = event.target;
-                      if (checkbox.checked) {
-                        updateSelectedItems();
-                        preventDeselection(checkbox);
-                      }
-                    });
+        document.querySelector('#coursesList').addEventListener('change', function(event) {
+        var checkbox = event.target;
+            if (checkbox.checked) {
+        updateSelectedItems();
+        preventDeselection(checkbox);
+        }
+        });
 </script>
