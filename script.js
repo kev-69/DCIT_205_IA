cd9a4da56416cdcf3f04072a89879e41a0fff310
kev-69 <bismarkobuobi19@gmail.com>
2023-11-25 21:20:24 +0000

    Added functionalities for the form and course selestion so that users would be prompted if their selection exceeds the maximum or is bellow expected.

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-<script>
-    var selectedItems = [];
-                  
-        function updateSelectedItems() {
-        selectedItems = [];
-        var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-        for (var i = 0; i < checkboxes.length; i++) {
-            if (checkboxes[i].checked) {
-        selectedItems.push(checkboxes[i].value);
-        }
-        }
-                    
-        }
-                  
-        function preventDeselection(checkbox) {
-            if (selectedItems.length >= 3 && !checkbox.checked) {
-        checkbox.checked = true;
-            alert('You can only select a maximum of three items.');
-        }
-                    
-        }
-                  
-        document.querySelector('#coursesList').addEventListener('change', function(event) {
-        var checkbox = event.target;
-            if (checkbox.checked) {
-        updateSelectedItems();
-        preventDeselection(checkbox);
-        }
-        });
-</script>
+function checkSelection() {
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    var notification = document.getElementById('notification');
+    var educationCheckboxes = document.querySelectorAll('input[name="education"]:checked');
+  
+    if (checkboxes.length < 4) {
+        notification.textContent = 'Please select at least 4 courses.';
+        notification.style.color = 'red';
+    } else if (checkboxes.length > 4) {
+        notification.textContent = 'Please select a maximum of 4 courses.';
+        notification.style.color = 'red';
+    } else {
+        notification.textContent = '';
+        // Add logic to handle the selected courses (e.g., submit form).
+    }
+
+    // Ensure only one option is selected for education
+    if (educationCheckboxes.length > 1) {
+        notification.textContent = 'Please select only one option for highest level of education.';
+        notification.style.color = 'red';
+    } else if (educationCheckboxes.length === 1 && checkboxes.length === 4) {
+        notification.textContent = ''; // Clear the notification if everything is valid
+    }
+}
