745a97d8e265f80ca430e35ce9e43d5af4fe7a18
kev-69 <bismarkobuobi19@gmail.com>
2023-11-26 20:02:18 +0000

    Added a funtion to help me disaply the map on the contact page

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ function checkSelection() {
     var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
     var notification = document.getElementById('notification');
     var educationCheckboxes = document.querySelectorAll('input[name="education"]:checked');
-  
+
+    // Validate checkboxes
     if (checkboxes.length < 4) {
         notification.textContent = 'Please select at least 4 courses.';
         notification.style.color = 'red';
@@ -11,14 +12,31 @@ function checkSelection() {
         notification.style.color = 'red';
     } else {
         notification.textContent = '';
-        // Add logic to handle the selected courses (e.g., submit form).
     }
 
-    // Ensure only one option is selected for education
+    // Validate education checkboxes
     if (educationCheckboxes.length > 1) {
         notification.textContent = 'Please select only one option for highest level of education.';
         notification.style.color = 'red';
     } else if (educationCheckboxes.length === 1 && checkboxes.length === 4) {
         notification.textContent = ''; // Clear the notification if everything is valid
     }
+
+    // Call the function to initialize the map
+    initializeMap();
+}
+
+function initializeMap() {
+    // Coordinates for your department's location
+    var departmentLocation = [12.345678, -45.678901];
+
+    // Create a map centered at the department's location
+    var map = L.map('map').setView(departmentLocation, 15);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+
+    L.marker(departmentLocation).addTo(map)
+        .bindPopup('Computer Science Department');
 }
